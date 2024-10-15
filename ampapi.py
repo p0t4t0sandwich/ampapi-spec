ce657a4995f3ec089867ce24c4308b79b22dbd2b
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-10-14 23:36:15 -0600

    Rewrote spec generation

---
+++ ampapi.py
@@ -0,0 +1,59 @@
+#!/bin/python3
+
+from __future__ import annotations
+
+import json
+import requests
+from typing import Any, Union
+
+class AMPAPI:
+    sessionId: str = ""
+    dataSource: str = ""
+    username: str = ""
+    password: str = ""
+
+    def __init__(self, baseUri: str) -> None:
+        if not baseUri[-1] == "/":
+            self.dataSource = baseUri + "/API"
+        else:
+            self.dataSource = baseUri + "API"
+
+    def APICall(self, endpoint: str, data: dict[str, Any] = {}) -> dict[str, Any]:
+        headers = {'Accept': 'text/javascript',}
+        session = {"SESSIONID": self.sessionId}
+        data_added = dict(session, **data)
+        data_json = json.dumps(data_added)
+
+        res = requests.post(
+            url=f"{self.dataSource}/{endpoint}",
+            headers=headers,
+            data=data_json
+        )
+        res_json = json.loads(res.content)
+
+        return res_json
+
+    def Login(self, username: str, password: str) -> dict[str, Any]:
+        self.username = username
+        self.password = password
+        res = self.APICall("Core/Login", {"username": username, "password": password, "token": "", "rememberMe": False})
+        if "success" in res.keys() and res["success"] == True:
+            self.sessionId = res["sessionID"]
+        return res
+
+    def ADSModule_GetInstances(self) -> dict[str, Any]:
+        return self.APICall("ADSModule/GetInstances")
+
+    def Core_GetAPISpec(self) -> dict[str, Any]:
+        return self.APICall("Core/GetAPISpec")
+
+    def Core_GetUpdateInfo(self) -> dict[str, Any]:
+        return self.APICall("Core/GetUpdateInfo")
+
+    def InstanceLogin(self, instance_id: str) -> Union[AMPAPI, None]:
+        new = AMPAPI(self.dataSource + f"/ADSModule/Servers/{instance_id}")
+        loginResult = new.Login(self.username, self.password)
+        if loginResult != None and "success" in loginResult.keys() and loginResult["success"] == True:
+            return new
+        else:
+            return None
--- validate_types.py
+++ validate_types.py
@@ -1,94 +1,32 @@
 #!/bin/python3
 
-from dataclasses import dataclass, field
+from __future__ import annotations
+
+from dataclasses import dataclass
 import json
-import requests
+from typing import Any
 
-cs_to_py = { # C# to Python
-        "Int32": "int",
-        "String": "str",
-        "Boolean": "bool",
-        "DateTime": "str",
-        "Guid": "str",
-        "List": "list",
-        "Dictionary": "dict",
-        "Object": "Any",
-        "Int64": "int",
-        "Double": "float",
-        "Float": "float",
-        "Void": "None",
-        "JSONRawResponse": "Any",
-    }
+from ampapi import AMPAPI
 
-class AMPAPI():
-    """Class for interacting with the AMP API"""
-    def __init__(self, baseUri: str) -> None:
-        """Initializes the AMP API class
-        :param baseUri: The base URI of the AMP instance
-        :type baseUri: str
-        :returns: None
-        """
-        self.baseUri = baseUri
-        self.sessionId = ""
-        self.dataSource = ""
-
-        if not self.baseUri[-1] == "/":
-            self.dataSource = self.baseUri + "/API"
-        else:
-            self.dataSource = self.baseUri + "API"
-
-    def APICall(self, endpoint: str, data: dict = {}) -> dict:
-        """Method to make AMP API calls
-        :param endpoint: The endpoint to call
-        :type endpoint: str
-        :param data: The data to send to the endpoint
-        :type data: dict
-        :value data: {}
-        :returns: dict with the result of the API call
-        :rtype: dict
-        """
-        headers = {'Accept': 'text/javascript',}
-        session = {"SESSIONID": self.sessionId}
-        data_added = dict(session, **data)
-
-        data_json = json.dumps(data_added)
-
-        res = requests.post(
-            url=f"{self.dataSource}/{endpoint}",
-            headers=headers,
-            data=data_json
-        )
-        res_json = json.loads(res.content)
-
-        return res_json
-    
-    def Login(self, username: str, password: str) -> None:
-        """Method to login to the AMP API
-        :param username: The username to login with
-        :type username: str
-        :param password: The password to login with
-        :type password: str
-        :returns: None
-        """
-        res = self.APICall("Core/Login", {"username": username, "password": password, "token": "", "rememberMe": False})
-        self.sessionId = res["sessionID"]
+# C# to Python
+cs_to_py = {
+    "Boolean": "bool",
+    "DateTime": "str",
+    "Dictionary": "dict",
+    "Double": "float",
+    "Float": "float",
+    "Guid": "str",
+    "Int32": "int",
+    "Int64": "int",
+    "JSONRawResponse": "Any",
+    "List": "list",
+    "Object": "Any",
+    "String": "str",
+    "Void": "None",
+}
 
 @dataclass
-class APIParameter():
-    Name: str
-    TypeName: str
-    Description: str
-    Optional: bool
-    ParamEnumValues: dict[str, int] = None
-
-@dataclass
-class APIMethod():
-    ReturnTypeName: str
-    IsComplexType: bool
-    Parameters: list[APIParameter] = field(default_factory=list)
-
-@dataclass
-class TypeField():
+class TypeField:
     Name: str
     TypeName: str
     Description: str
@@ -96,29 +34,22 @@ class TypeField():
     Value: int = None
 
 @dataclass
-class TypeDef():
+class TypeDef:
     Description: str
     Fields: list[TypeField]
     SpecialNote: str = None
     SpecialType: str = None
     SerializesAs: str = None
 
-class UpdateTypes():
-    APISpec: dict[str, dict[str, APIMethod]] = {}
+class UpdateTypes:
+    APISpec: dict[str, dict[str, Any]] = {}
     TypeSpec: dict[str, TypeDef] = {}
 
     def _load_api_spec(self) -> None:
         with open("./TypedAPISpec.json") as f:
             json_str: str = f.read()
-            for module_name, module in json.loads(json_str).items():
-                if module_name not in self.APISpec:
-                    self.APISpec[module_name] = {}
-                for method_name, method in module.items():
-                    self.APISpec[module_name][method_name] = APIMethod(
-                        Parameters=[APIParameter(**param) for param in method["Parameters"]],
-                        ReturnTypeName=method["ReturnTypeName"],
-                        IsComplexType=method["IsComplexType"]
-                    )
+            self.APISpec = json.loads(json_str)
+            f.close()
 
     def _load_type_spec(self) -> None:
         with open("./TypeSpec.json") as f:
@@ -135,20 +66,6 @@ class UpdateTypes():
     def __init__(self) -> None:
         self._load_api_spec()
         self._load_type_spec()
-
-    def print_api_spec(self) -> None:
-        for module_name, methods in self.APISpec.items():
-            for method_name, method in methods.items():
-                print(f"{module_name}.{method_name}")
-                print(f"  ReturnTypeName: {method.ReturnTypeName}")
-                print(f"  IsComplexType: {method.IsComplexType}")
-                for param in method.Parameters:
-                    print(f"  Parameter: {param.Name}")
-                    print(f"    TypeName: {param.TypeName}")
-                    print(f"    Description: {param.Description}")
-                    print(f"    Optional: {param.Optional}")
-                    if param.ParamEnumValues:
-                        print(f"    EnumValues: {param.ParamEnumValues}")
 
     def convert_type(self, type_name: str) -> str:
         if type_name in cs_to_py:
@@ -216,7 +133,7 @@ class UpdateTypes():
 
     def validate_response(self, module: str, method: str, response: dict) -> bool:
         # Look up return type
-        return_type = self.APISpec[module][method].ReturnTypeName
+        return_type = self.APISpec[module][method]["ReturnTypeName"]
 
         # Generate return object structure
         type_graph: dict | list = self.lookup_type(return_type)
